257503b94ad0177696083ac88ddf7a456f0b7ea9
Alex Levine <a17levine@gmail.com>
2014-09-26 01:16:14 -0500

    got info to post to the server, big win

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -4,6 +4,12 @@ class PlacesController < ApplicationController
   end
 
   def create
+  	@place = current_user.places.new(place_params)
+  	if @place.save
+  		redirect_to current_user
+  	else
+  		#return an error
+  	end
   end
 
   def edit
@@ -11,4 +17,14 @@ class PlacesController < ApplicationController
 
   def destroy
   end
+
+	private
+    def place_params
+      params.require(:place).permit(:alias, 
+      															:name, 
+      															:google_friendly_name, 
+      															:formatted_address,
+      															:lat_long,
+      															:notes)
+    end
 end
